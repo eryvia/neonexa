72260fe97c8b0d9fd66e22266dd1a6eb635b11ba
Vojtěch Peňák <penakvo22@sps-prosek.lad>
2025-10-21 13:42:16 +0200

    added currency sprite + updated movement and double jump

--- player.gd
+++ player.gd
@@ -1,27 +1,32 @@
 extends CharacterBody2D
 
-const SPEED = 200
-const JUMP_VELOCITY = -400.0
+@export var speed = 200
+@export var jumpVelocity:float = -230.0
+@export var dashVelocity:float = -500.0
+@export var hp:int = 3
+@export var is_midair:bool = false
+var jumps = 2;
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
+		is_midair = true
 		velocity += get_gravity() * delta
+	else: 
+		jumps = 2
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actiaons.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_just_pressed("jump") and jumps != 0:
+		velocity.y = jumpVelocity
+		jumps -= 1
+
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_h = direction < 0
 		$AnimatedSprite2D.play()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 		$AnimatedSprite2D.animation = "idle"
 		$AnimatedSprite2D.play()
 		
