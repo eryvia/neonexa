49da7cf6112756f08b399ce3b78511f9288403f1
eryvia <eryvia.reborn@gmail.com>
2026-02-24 22:37:54 +0100

    add new sprites, new anim, new main_screen

--- Stages/MainScene/mainScene.gd
+++ Stages/MainScene/mainScene.gd
@@ -4,12 +4,16 @@ var hovering: bool = false
 var button_type = null
 const sandbox = preload(Global.GAME_SCENES.sandbox)
 @onready var fade_timer = $FadeTransition/FadeTimer
+@onready var menu_items := $MainVBox.get_children()
+@onready var mini_axo := $AnimatedSprite2D
+var selected_index := 0
 
 var cursor1
 var cursor2
 #Input.set_custom_mouse_cursor(cursor1)
 
 func _ready() -> void:
+	update_menu()
 	fade_timer.wait_time = 1.0
 	pass
 
@@ -24,11 +28,54 @@ func _on_exit_pressed() -> void:
 	$FadeTransition.show()
 	fade_timer.start()
 	$FadeTransition/AnimationPlayer.play("fade_in")
-	
-	
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_down"):
+		selected_index += 1
+		if selected_index >= menu_items.size():
+			selected_index = 0
+		update_menu()
+
+	elif event.is_action_pressed("ui_up"):
+		selected_index -= 1
+		if selected_index < 0:
+			selected_index = menu_items.size() - 1
+		update_menu()
+
+	elif event.is_action_pressed("ui_accept"):
+		activate_selected()
+
+func update_menu():
+	for i in range(menu_items.size()):
+		if i == selected_index:
+			menu_items[i].modulate = Color(1, 1, 0)
+
+			mini_axo.global_position.y = menu_items[i].global_position.y
+			mini_axo.global_position.x = menu_items[i].global_position.x - 24
+
+			mini_axo.play("default")
+		else:
+			menu_items[i].modulate = Color(1, 1, 1)
+
 func _on_fade_timer_timeout() -> void:
 	if button_type == "start":
 		get_tree().change_scene_to_packed(sandbox)
 		pass
 	if button_type == "exit":
 		get_tree().quit()
+
+func activate_selected():
+	match selected_index:
+		0:
+			button_type = "start"
+			$FadeTransition.show()
+			fade_timer.start()
+			$FadeTransition/AnimationPlayer.play("fade_in")
+		1:
+			print("Options")
+		2:
+			button_type = "exit"
+			$FadeTransition.show()
+			fade_timer.start()
+			$FadeTransition/AnimationPlayer.play("fade_in")
+			
